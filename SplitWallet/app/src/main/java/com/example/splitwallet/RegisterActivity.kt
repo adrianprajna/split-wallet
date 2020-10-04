d6b42c91d79f15d53445bb6594ab154e513e9ccf
ferdinandg066 <ferdinandg066@gmail.com>
2020-10-04 20:16:25 +0700

    Input new user to firebase

--- SplitWallet/app/src/main/java/com/example/splitwallet/RegisterActivity.kt
+++ SplitWallet/app/src/main/java/com/example/splitwallet/RegisterActivity.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,16 +20,25 @@ class RegisterActivity : AppCompatActivity() {
         var btn_login = findViewById<Button>(R.id.loginButton)
 
         btn_login.setOnClickListener(){
-            var et_username = findViewById<EditText>(R.id.username).text
-            var et_email = findViewById<EditText>(R.id.email).text
-            var et_password = findViewById<EditText>(R.id.passwordLogin).text
+            var et_username = findViewById<EditText>(R.id.username).text.toString()
+            var et_email = findViewById<EditText>(R.id.email).text.toString()
+            var et_password = findViewById<EditText>(R.id.passwordLogin).text.toString()
 
             if (!et_username.isEmpty() && !et_password.isEmpty() && !et_email.isEmpty()){
-                Toast.makeText(this, R.string.success_register_toast, Toast.LENGTH_SHORT).show()
+                createUser(et_username, et_email, et_password)
             } else {
                 Toast.makeText(this, R.string.empty_field_toast, Toast.LENGTH_SHORT).show()
             }
         }
     }
+
+    fun createUser(username : String, email : String, password : String){
+        var reff : DatabaseReference
+        reff = FirebaseDatabase.getInstance().getReference().child("Users");
+
+        var users = Users(username, password, email)
+        reff.push().setValue(users)
+    }
+
 
 }
---
+++ SplitWallet/app/src/main/java/com/example/splitwallet/Users.kt
@@ -0,0 +1,6 @@
+package com.example.splitwallet
+
+class Users (var username: String,var password: String, var email: String) {
+
+
+}
