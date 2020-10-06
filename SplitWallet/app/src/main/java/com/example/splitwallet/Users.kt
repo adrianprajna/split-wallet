060558577be25f0670ad4a765ab6923cdc03af64
adrianprajna <65269354+adrianprajna@users.noreply.github.com>
2020-10-06 13:28:49 +0700

    follow up

--- SplitWallet/app/src/main/java/com/example/splitwallet/Users.kt
+++ SplitWallet/app/src/main/java/com/example/splitwallet/Users.kt
@@ -1,6 +1,14 @@
 package com.example.splitwallet
 
-class Users (var username: String,var password: String, var email: String) {
+class Users {
+    var username: String? = ""
+    var password: String? = ""
+    var email: String? = ""
 
-
+    constructor(){}
+    constructor(username: String?, password:String?, email:String?){
+        this.username = username
+        this.password = password
+        this.email = email
+    }
 }
