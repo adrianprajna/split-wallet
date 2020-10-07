fa291a03961f6a5c17928eeb8a4338cfd8bbfeff
adrianprajna <65269354+adrianprajna@users.noreply.github.com>
2020-10-07 17:39:55 +0700

    add report fragment

--- SplitWallet/app/src/main/java/com/example/splitwallet/adapter/PageAdapter.kt
+++ SplitWallet/app/src/main/java/com/example/splitwallet/adapter/PageAdapter.kt
@@ -3,25 +3,23 @@ package com.example.splitwallet.adapter
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentStatePagerAdapter
-import com.example.splitwallet.fragments.AccountFragment
-import com.example.splitwallet.fragments.CalendarFragment
-import com.example.splitwallet.fragments.HomeFragment
-import com.example.splitwallet.fragments.Transaction
+import com.example.splitwallet.fragments.*
 
 class PageAdapter(var fm: FragmentManager): FragmentStatePagerAdapter(fm){
 
     override fun getItem(position: Int): Fragment {
         return when(position){
             0 -> HomeFragment()
-            1 -> Transaction()
-            2 -> CalendarFragment()
-            3 -> AccountFragment()
+            1 -> CalendarFragment()
+            2 -> Transaction()
+            3 -> ReportFragment()
+            4 -> AccountFragment()
             else -> HomeFragment()
         }
     }
 
     override fun getCount(): Int {
-        return 4
+        return 5
     }
 
 
