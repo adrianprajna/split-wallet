da8b7297f4cf2fc88126bcea649771ff6060d9ab
ferdinandg066 <ferdinandg066@gmail.com>
2020-10-12 11:26:35 +0700

    Report complete

--- SplitWallet/app/src/main/java/edu/bluejack20_1/splitwallet/support_class/Constants.kt
+++ SplitWallet/app/src/main/java/edu/bluejack20_1/splitwallet/support_class/Constants.kt
@@ -8,6 +8,12 @@ object Constants {
     const val LIST_WALLET = "listWallets"
     lateinit var KEY_USER_ID : String
 
+    const val NOTIFICATION_STATUS = "notificationStatus"
+    const val THEME_STATUS = "themeStatus"
+
+    const val THEME_LIGHT = "LIGHT"
+    const val THEME_DARK = "DARK"
+
     fun capitalizeEachWord(string : String) : String{
         return string.split(" ").map { it.capitalize() }.joinToString(" ")
     }
